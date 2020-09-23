df8ff8a84a0769ff00b63fa47a0b2bc71dc1f3f3
Dương Bảo Hưng <duongbaohung@Duongs-MacBook-Pro.local>
2020-09-23 17:39:32 +0700

    finish basic login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import {
-  ColorModeProvider,
-  CSSReset,
-  Text,
-  ThemeProvider,
-} from "@chakra-ui/core";
+import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core";
 import React from "react";
 import { SignInLayout } from "./layout/SignIn/SignIn";
-import { Header } from "./section/Header/Header";
 import { customTheme } from "./theme";
 function App() {
   return (
     <ThemeProvider theme={customTheme}>
       <ColorModeProvider>
         <CSSReset />
-        <Header />
         <SignInLayout />
-        <Text my={5} textAlign="center">
-          Create React App Chakra UI
-        </Text>
       </ColorModeProvider>
     </ThemeProvider>
   );
--- src/layout/SignIn/SignIn.js
+++ src/layout/SignIn/SignIn.js
@@ -1,11 +1,17 @@
-import { Flex } from "@chakra-ui/core";
+import { Flex, Text } from "@chakra-ui/core";
 import React from "react";
 import { LoginForm } from "../../section/LoginForm/LoginForm";
-
+import { Header } from "../../section/Header/Header";
 export const SignInLayout = () => {
   return (
-    <Flex my={50} width="100%" textAlign="center" justify="center">
-      <LoginForm />
-    </Flex>
+    <>
+      <Header />
+      <Flex my={50} width="100%" textAlign="center" justify="center">
+        <LoginForm />
+      </Flex>
+      <Text my={5} textAlign="center">
+        Create React App Chakra UI
+      </Text>
+    </>
   );
 };
